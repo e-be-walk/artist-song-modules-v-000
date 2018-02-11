88f49a2d33b537976b9759111cef632039f58bff
Erin Walker <erinb0317@hotmail.com>
2018-02-11 00:50:47 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,7 @@ class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   extend Findable
+  include Paramable
 
   attr_accessor :name
   attr_reader :songs
@@ -27,10 +28,6 @@ class Artist
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 
 end
